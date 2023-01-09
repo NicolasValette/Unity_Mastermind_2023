9ff0bd6dd25a16187cdebef82b25c98f60211830
NikoMyoji <nicolas.vlte@gmail.com>
2023-01-09 12:58:17 +0100

    Change pawn color

---
+++ Assets/Scripts/PawnController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawnController : MonoBehaviour
+{
+    [SerializeField]
+    private Material[] _materials;           //List of all possible colors
+
+    private MeshRenderer _color;
+    private int _actualIndColor = 0;       //Indice of the actual color
+    // Start is called before the first frame update
+    void Start()
+    {
+        _color= GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ChangeColor();
+        }
+        
+    }
+
+    private void ChangeColor()
+    {
+        _actualIndColor = _actualIndColor >= _materials.Length -1 ? 0 : _actualIndColor +1;
+        _color.material = _materials[_actualIndColor];
+    }
+}
