945fde8ac811b7943e67ef82ddabbba6921c33dc
NikoMyoji <nicolas.vlte@gmail.com>
2023-01-12 14:02:13 +0100

    Drag & Drop fix

--- Assets/Scripts/PawnController.cs
+++ Assets/Scripts/PawnController.cs
@@ -10,7 +10,7 @@ public class PawnController : MonoBehaviour
     //[SerializeField]
     //private Material[] _materials;           //List of all possible colors
     [SerializeField]
-    private GameObject _MastermindManager;
+    private GameObject _mastermindManager;
     [SerializeField]
     private bool _isPickable = false;
     [SerializeField]
@@ -39,6 +39,7 @@ public class PawnController : MonoBehaviour
             return _color.material;
         }
     }
+    private bool _isActive;
     public bool IsActive
     {
         get
@@ -85,10 +86,12 @@ public class PawnController : MonoBehaviour
         if (!_isPickable)
         {
             _collider.enabled = false;
+            
         }
-        _gameManager = _MastermindManager.GetComponent<MastermindManager>();
+        _gameManager = _mastermindManager.GetComponent<MastermindManager>();
         _state = new NoDraggablePawnState();
         _initialPosition = transform.position;
+        _isActive = false;
     }
     // Update is called once per frame
     void Update()
@@ -96,6 +99,10 @@ public class PawnController : MonoBehaviour
         Move();
     }
 
+    public void Initialisation (MastermindManager mastermind)
+    {
+        _gameManager = mastermind;
+    }
     public void ChangeColor()
     {
         _actualIndColor = _actualIndColor >= _gameManager.Colors.Length - 1 ? 0 : _actualIndColor + 1;
@@ -116,6 +123,7 @@ public class PawnController : MonoBehaviour
     public void setActive(bool activity)
     {
         _collider.enabled = activity;
+        _isActive= activity;
         if (activity)
         {
             _color.material = _gameManager.ActivePawnColor;
@@ -138,9 +146,11 @@ public class PawnController : MonoBehaviour
     {
         Cursor.visible = false;
         GameObject newPawn = Instantiate(hit.transform.gameObject, Input.mousePosition, Quaternion.identity);
+        newPawn.GetComponent<PawnController>().Initialisation(_gameManager);
         newPawn.GetComponent<PawnController>().SwitchState();
         Debug.Log($"new Pawn instanciate name : {newPawn.name}, state : {newPawn.GetComponent<PawnController>().State}");
-        newPawn.GetComponent<PawnController>()._collider.enabled = false;
+        //newPawn.GetComponent<PawnController>()._collider.enabled = false;
+        newPawn.layer = LayerMask.NameToLayer("Ignore Raycast");
         return newPawn;
     }
     public void Drop()
@@ -155,6 +165,7 @@ public class PawnController : MonoBehaviour
                 hit.transform.gameObject.SetActive(false);
                 hit.transform.parent.GetComponent<RowController>()?.ChangePawn(hit.transform.gameObject.GetComponent<PawnController>().PositionInd, transform.gameObject);
                 _state.Release(transform, hit.transform.position);
+                gameObject.layer = 0;
             }
             else
             {
